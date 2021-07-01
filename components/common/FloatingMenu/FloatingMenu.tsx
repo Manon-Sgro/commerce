c8b7951e767b5186af982908e91fb874363c0de2
Manon <manon-sgro@outlook.fr>
2021-07-01 12:48:12 +0200

    feat: add menu animations

---
+++ components/common/FloatingMenu/FloatingMenu.tsx
@@ -0,0 +1,17 @@
+import { FC, useEffect, useMemo, useState } from 'react'
+import cn from 'classnames'
+import s from './FloatingMenu.module.css'
+import { useRouter } from 'next/router'
+import { Button } from '../../ui'
+import Link from 'next/link'
+
+interface Props {
+  className?: string
+  id?: string
+}
+
+const FloatingMenu: FC<Props> = ({ className, id = 'search' }) => {
+  return <div className={`${className} ${s.root}`}>Hello!</div>
+}
+
+export default FloatingMenu
