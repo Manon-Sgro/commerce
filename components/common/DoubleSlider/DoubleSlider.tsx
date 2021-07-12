69b77c32791c4a5a18ecb3be05994af41e74502d
Manon <manon-sgro@outlook.fr>
2021-07-12 08:44:08 +0200

    fix: double range slider

--- components/common/DoubleSlider/DoubleSlider.tsx
+++ components/common/DoubleSlider/DoubleSlider.tsx
@@ -1,83 +1,68 @@
-import cn from 'classnames'
-import s from './DoubleSlider.module.css'
-import { useRouter } from 'next/router'
-import {
+import React, {
+  ChangeEvent,
   FC,
-  ReactNode,
-  Component,
-  useMemo,
-  useState,
-  useRef,
   useCallback,
   useEffect,
+  useState,
+  useRef,
 } from 'react'
+import s from './DoubleSlider.module.css'
 
-interface Props {
-  className?: string
-  id?: string
+interface MultiRangeSliderProps {
   min: number
   max: number
   onChange: Function
 }
 
-const DoubleSlider: FC<Props> = ({
-  className,
-  id,
-  min = 0,
-  max = 1000,
-  onChange,
-}) => {
-  const [minValue, setMinValue] = useState(min)
-  const [maxValue, setMaxValue] = useState(max)
-
+const DoubleSlider: FC<MultiRangeSliderProps> = ({ min, max, onChange }) => {
+  const [minVal, setMinVal] = useState(min)
+  const [maxVal, setMaxVal] = useState(max)
   const minValRef = useRef(min)
   const maxValRef = useRef(max)
-  const range = useRef(null)
+  const range = useRef<HTMLDivElement>(null)
 
   // Convert to percentage
   const getPercent = useCallback(
-    (value) => {
-      Math.round(((value - min) / (max - min)) * 100)
-    },
+    (value: number) => Math.round(((value - min) / (max - min)) * 100),
     [min, max]
   )
 
-  // Set width of the range to change from the left side
+  // Set width of the range to decrease from the left side
   useEffect(() => {
-    const minPercent = getPercent(minValue)
+    const minPercent = getPercent(minVal)
     const maxPercent = getPercent(maxValRef.current)
 
     if (range.current) {
       range.current.style.left = `${minPercent}%`
       range.current.style.width = `${maxPercent - minPercent}%`
     }
-  }, [minValue, getPercent])
+  }, [minVal, getPercent])
 
-  // Set width of the range to change from the right side
+  // Set width of the range to decrease from the right side
   useEffect(() => {
     const minPercent = getPercent(minValRef.current)
-    const maxPercent = getPercent(maxValue)
+    const maxPercent = getPercent(maxVal)
 
     if (range.current) {
       range.current.style.width = `${maxPercent - minPercent}%`
     }
-  }, [maxValue, getPercent])
+  }, [maxVal, getPercent])
 
   // Get min and max values when their state changes
   useEffect(() => {
-    onChange({ min: minValue, max: maxValue })
-  }, [minValue, maxValue, onChange])
+    onChange({ min: minVal, max: maxVal })
+  }, [minVal, maxVal, onChange])
 
   return (
-    <div className={s.section}>
+    <div className={s.container}>
       <input
         type="range"
         min={min}
         max={max}
-        value={minValue}
-        onChange={(event) => {
-          const value = Math.min(Number(event.target.value), maxValue - 1)
-          setMinValue(value)
+        value={minVal}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.min(Number(event.target.value), maxVal - 1)
+          setMinVal(value)
           minValRef.current = value
         }}
         className={`${s.thumb} ${s.thumb__left}`}
@@ -86,18 +71,20 @@ const DoubleSlider: FC<Props> = ({
         type="range"
         min={min}
         max={max}
-        onChange={(event) => {
-          const value = Math.max(Number(event.target.value), minValue + 1)
-          setMaxValue(value)
+        value={maxVal}
+        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+          const value = Math.max(Number(event.target.value), minVal + 1)
+          setMaxVal(value)
           maxValRef.current = value
         }}
         className={`${s.thumb} ${s.thumb__right}`}
       />
-      <div className={`${s.slider}`}>
-        <div className={`${s.slider__track}`} />
-        <div ref={range} className="slider__range" />
-        <div className={s.slider__left__value}>{minValue}</div>
-        <div className={s.slider__right__value}>{maxValue}</div>
+
+      <div className={s.slider}>
+        <div className={s.slider__track}></div>
+        <div ref={range} className={s.slider_range}></div>
+        <div className={s.slider__left__value}>{minVal}</div>
+        <div className={s.slider__right__value}>{maxVal}</div>
       </div>
     </div>
   )
